7b5360984e95ebbf39e1d39ade52694d23035d74
Mars Zuo <marszuo@foxmail.com>
2023-10-21 23:35:31 +0800

    trivial: A little prompt adjustment

--- main.cc
+++ main.cc
@@ -6,8 +6,8 @@ int main(int argc, char *argv[]) {
     std::string address = "127.0.0.1:8080";
     std::string allow_cmd_dir = "/home";
     auto cli = (
-        clipp::option("-addr").doc("Address") & clipp::value("address", address),
-        clipp::option("-allow_cmd_dir").doc("Allow") & clipp::value("allow_cmd_dir", allow_cmd_dir)
+        clipp::option("-addr").doc("address") & clipp::value("address", address),
+        clipp::option("-allow_cmd_dir").doc("allow cmd dir") & clipp::value("allow_cmd_dir", allow_cmd_dir)
     );
     parse(argc, argv, cli);
 
